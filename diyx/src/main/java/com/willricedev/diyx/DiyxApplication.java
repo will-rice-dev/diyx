7ab279fe975d7d47fe68355b0ce26847c0c4eba8
Will Rice <will.rice.dev@gmail.com>
2026-02-13 20:36:43 -0500

    init

---
+++ diyx/src/main/java/com/willricedev/diyx/DiyxApplication.java
@@ -0,0 +1,13 @@
+package com.willricedev.diyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyxApplication.class, args);
+	}
+
+}
---
+++ diyx/src/test/java/com/willricedev/diyx/DiyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willricedev.diyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
